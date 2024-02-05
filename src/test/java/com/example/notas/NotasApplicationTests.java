ae9b770efaff5e420a5bb0d06821407ae0552657
gabrielcsaturnino <tuladel.123.team2020@hotmail.com>
2024-02-05 10:55:49 -0300

    teste

--- src/test/java/com/example/notas/NotasApplicationTests.java
+++ src/test/java/com/example/notas/NotasApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.notas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*TESTE GIT */
 @SpringBootTest
 class NotasApplicationTests {
 
