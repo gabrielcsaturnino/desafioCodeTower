dbf7908ab9aa0e0df95b3719bcc7e1d545c6821f
gabrielcsaturnino <tuladel.123.team2020@hotmail.com>
2024-02-08 10:54:48 -0300

    dd

--- src/main/java/com/example/notas/controller/NotaController.java
+++ src/main/java/com/example/notas/controller/NotaController.java
@@ -4,7 +4,6 @@ import com.example.notas.model.Nota;
 import com.example.notas.dto.NotaRequestDTO;
 import com.example.notas.services.NotaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/example/notas/model/Nota.java
+++ src/main/java/com/example/notas/model/Nota.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 
+
 @Entity @Getter @Setter @Table(name = "Nota")
 public class Nota {
 
